c34c8aa9c022ac33050249172d13e02f1cd266a2
javieranguloluna <54935581+javieranguloluna@users.noreply.github.com>
2022-11-24 14:33:15 +0100

    contact model

---
+++ src/models/contact.model.js
@@ -0,0 +1,8 @@
+export default class Contact {
+    constructor(firstName, lastName, email) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email = email
+        this.connected = true
+    }
+}

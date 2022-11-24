c497c663bc400f49570743bb8bde6a25b913542a
javieranguloluna <54935581+javieranguloluna@users.noreply.github.com>
2022-11-24 14:32:55 +0100

    addedcontact list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ContactsList from './components/container/contacstList'
 
 function App() {
   return (
     <div className="App">
-
+      <ContactsList></ContactsList>
     </div>
   );
 }
